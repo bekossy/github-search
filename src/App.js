2a59bfd42f9ca0b3b91b152b3b3609e70ec9e26b
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-08-01 19:54:47 +0100

    imports Header file and sets parent component for document

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./title/Header";
+
+const App = () => {
+  return (
+    <>
+      <section className="container">
+        <Header />
+      </section>
+    </>
+  );
+};
+
+export default App;
