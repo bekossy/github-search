8f7cc74149bfa8dfae504f2bfc75e0daca690023
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-08-03 06:59:44 +0100

    sets state for the loading and error component

--- src/searchBar/Search.js
+++ src/searchBar/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Layout } from "../layout/Layout";
 import { Loading } from "../layout/Loading";
+import { Error } from "../layout/Error";
 
 const url = "https://api.github.com/users/";
 export const Search = () => {
@@ -14,20 +15,26 @@ export const Search = () => {
     console.log(url + text);
     if (text) {
       setIsLoading(true);
+      setIsError(false);
       fetch(url + text)
         .then((resp) => {
           if (resp.status >= 200 && resp.status <= 299) {
             return resp.json();
           } else {
-            throw new Error(resp.statusText);
+            setIsError(true);
+            setIsLoading(false);
           }
         })
         .then((user) => {
           console.log(user);
           setIsLoading(false);
+          setIsError(false);
           return <Layout {...user} key={user.id} />;
         })
-        .catch((error) => console.log(error));
+        .catch(() => {
+          setIsError(true);
+          setIsLoading(false);
+        });
     }
 
     setText("");
@@ -38,7 +45,7 @@ export const Search = () => {
       <form className="form" onSubmit={handleSubmit}>
         <svg style={{ width: "30px", height: "30px" }} viewBox="0 0 24 24">
           <path
-            fill="#134ebd"
+            fill="#2a68db"
             d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
           />
         </svg>
@@ -53,6 +60,7 @@ export const Search = () => {
         </button>
       </form>
       {isLoading && <Loading />}
+      {isError && <Error />}
     </>
   );
 };
