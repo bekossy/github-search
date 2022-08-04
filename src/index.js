e8466f109e89b39f64fbf687fbf7473196552735
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-08-04 01:09:05 +0100

    changes render to createRoot in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import ReactDom from "react-dom";
+import * as ReactDom from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDom.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = ReactDom.createRoot(container);
+
+root.render(<App />);
